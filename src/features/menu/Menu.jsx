07542cd94d512326f3442673c0b14169a2ce1ba4
Helio Dourado <heliohdd@yahoo.com.br>
2025-01-08 17:31:04 -0300

    Section 22.288 - Displaying a Loading Indicator

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable no-unused-vars */
 import { useLoaderData } from "react-router-dom";
 import { getMenu } from "../../services/apiRestaurant";
 import MenuItem from "./MenuItem";
+import { useState } from "react";
 
 function Menu() {
   const menu = useLoaderData();
-  console.log(menu);
 
   return (
     <ul>
