1e15fbc92e3ec2c7948b22ea263a22e0c76725d3
Helio Dourado <heliohdd@yahoo.com.br>
2025-01-08 16:57:05 -0300

    Section 22.287 - Fetching Data With React Router 'Loaders': Pizza Menu

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,3 +1,8 @@
+/* eslint-disable no-unused-vars */
+
+import { formatCurrency } from "../../utils/helpers";
+
+/* eslint-disable react/prop-types */
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
@@ -6,7 +11,7 @@ function MenuItem({ pizza }) {
       <img src={imageUrl} alt={name} />
       <div>
         <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
+        <p>{ingredients.join(", ")}</p>
         <div>
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
         </div>
